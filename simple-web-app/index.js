0b850063e06b108aa44651408a6f59c165bf76c7
leandrofahur <leandrofahur@gmail.com>
2020-08-26 13:52:20 -0700

    cache busting and rebuilds

--- simple-web-app/index.js
+++ simple-web-app/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello there!");
+  res.send("How are you doing?");
 });
 
 app.listen(8080, () => {
